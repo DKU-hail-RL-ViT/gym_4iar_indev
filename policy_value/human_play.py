9b559016c44366e3e21cc306a5d7b6c7da445766
dongjaekim-hail <dongjaekim@dankook.ac.kr>
2024-05-12 15:32:31 +0900

    edit human_play

--- policy_value/human_play.py
+++ policy_value/human_play.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 from fiar_env import Fiar, action2d_ize
 from policy_value_network import PolicyValueNet
 from mcts import MCTSPlayer
-# from elo import define_players
 
 import numpy as np
 
@@ -25,13 +24,18 @@ def check_game_type(env, player1, human):
         check_game_type(env, player1, human)
 
 
-def location_to_move(location, board_width=9, board_height=4):
+def location_to_move(env, location, board_width=9, board_height=4):
+    # map = np.int16(np.linspace(0, 4 * 9 - 1, 4 * 9).reshape(9, 4))
+    # action2d = np.where(map == action)
     if len(location) != 2:
         return -1
+
     h = location[0]
     w = location[1]
-
-    move = board_height * w + h
+    if h > 8 or w > 3:
+        print("invaild action")
+        Human.get_action(env)
+    move = board_height * h + w
     if move not in range(board_width * board_height):
         return -1
     return move
@@ -55,7 +59,7 @@ def start_play(env, player1, player2, human_index=None):
         if is_human_index:
             move = player_in_turn.get_action(env)
         else:
-            move = player_in_turn.get_action(env, temp=1e-6, return_prob=0)
+            move = player_in_turn.get_action(env, temp=1e-4, return_prob=0)
 
         obs, reward, terminated, info = env.step(move)
         assert env.state_[3][action2d_ize(move)] == 1, ("Invalid move", action2d_ize(move))
@@ -65,9 +69,11 @@ def start_play(env, player1, player2, human_index=None):
             current_player = 1 - current_player
             is_human_index = (current_player == human_index)
             player_in_turn = players[current_player]
+            if not is_human_index:
+                player_in_turn.oppo_node_update(move)  # [ToDo] 여기 수정 해야 함
             if current_player == 0:
                 pass
-                # player_in_turn.oppo_node_update(move)  # [ToDo] 여기 수정 해야 함
+
         else:
             print(env)
             obs, _ = env.reset()
@@ -96,58 +102,50 @@ class Human(object):
         availables = [i for i in range(36) if not np.any(env.state()[3][i // 4][i % 4] == 1)]
         try:
             print(env)
-            location = input("Your move (max35 = 3,8) : ")
+            location = input("Your move (max35 = 8,3 (Row, Col) [transition tuple]) : ")
             if isinstance(location, str):
                 location = [int(n, 10) for n in location.split(",")]
-            move = location_to_move(location)
+            move = location_to_move(env, location)
         except Exception as e:
             move = -1
         if move == -1 or move not in availables:
             print("invalid move")
-            move = self.get_action(env.state_)
+            self.get_action(env.state_)
         return move
+
+    def oppo_node_update(self, move):
+        pass
 
     def __str__(self):
         return "Human {}".format(self.player)
 
 
-def run(env, ):
+def run(env, rl_model, n_playout, file_num, quantile=32, start_player=0):
 
     if rl_model == "AC":
         model_file = f"Eval/{rl_model}_nmcts{n_playout}/train_{file_num:03d}.pth"
     else:
         model_file = f"Eval/{rl_model}_nmcts{n_playout}_quantiles{quantile}/train_{file_num:03d}.pth"
 
-    try:
-        best_policy = PolicyValueNet(env.state().shape[1], env.state().shape[2], quantile,
-                                     model_file=model_file, rl_model=rl_model)
 
-        mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5,
-                                 n_playout=n_playout)  # set larger n_playout for better performance
+    best_policy = PolicyValueNet(env.state().shape[1], env.state().shape[2], quantile,
+                                 model_file=model_file, rl_model=rl_model)
 
+    mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5,
+                             n_playout=n_playout)  # set larger n_playout for better performance
 
-        if start_player == 0:  # set start_player=0 for human first
-            player = 0
-            start_play(env, human, mcts_player, player)
-        else:
-            player = 1
-            start_play(env, mcts_player, human, player)
-
-    except KeyboardInterrupt:
-        print('\n\rquit')
-
+    return mcts_player
 
 if __name__ == '__main__':
     env = Fiar()
 
-    p1_rl_model = "AC"
+    p1_rl_model = "QRAC"
     p1_n_playout = 400
-    p1_quantiles = 32
-    p1_file_num = 40
-    start_player = 0  # start_player 0 : human(black), start_player 1 : human(white)
+    p1_quantiles = 2
+    p1_file_num = 100
 
     # human player, input your move in the format: 2,3
     human = Human()
 
-    player1 = define_players(p1_rl_model, p1_n_playout, p1_file_num, p1_quantiles)
+    player1 = run(env, p1_rl_model, p1_n_playout, p1_file_num, p1_quantiles)
     check_game_type(env, player1, human)
